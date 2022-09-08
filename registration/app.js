bc16fadba425802f8287e8eef66bdd1566c4e304
Ashish Shukla <akshkgd@gmail.com>
2022-09-08 05:35:43 +0530

    wd

--- registration/app.js
+++ registration/app.js
@@ -16,7 +16,43 @@ function register(){
 else{
     console.log('user already exist')
 }
-    console.log(users);
     document.getElementById('name').value = '';
     document.getElementById('email').value = ''; 
+    let container = document.getElementById('userContainer');
+    container.innerHTML = '';
+    users.map((user)=>{
+        
+        let div = document.createElement('div');
+        div.classList.add('user-card');
+        let name = document.createElement('h4');
+        let email = document.createElement('p');
+        name.innerHTML = user.name;
+        email.innerHTML = user.email;
+        container.appendChild(div)
+        div.appendChild(name);
+        div.appendChild(email)
+    })
+}
+
+
+users.map((x)=>{
+    let div = document.createElement('div');
+    container.appendChild(div)
+})
+
+function checkEmail(){
+    let tempEmail = document.getElementById('email').value;
+
+   let emailExist = users.filter((user)=>{
+        return user.email == tempEmail
+    })
+    let alert = document.getElementById('alert')
+    if(emailExist.length == 0){
+        alert.classList.add('d-none');
+        alert.classList.remove('danger');
+    }
+    else{
+        alert.classList.remove('d-none');
+        alert.classList.add('danger');
+    }
 }
---
+++ registration/doubts.js
@@ -0,0 +1,4 @@
+// js datatypes => string, numbers, boolean, null, objects
+let a = []
+let b = {}
+console.log( typeof b )
